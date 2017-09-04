7ade21b81322c8ffce0be4b6592ed4c6e9bcddfe
Isaac Ludlow <isaac@theludlows.org>
2017-09-04 10:23:37 -0400

    Added headers

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.use(require('./middleware/headers'))
+
+app.use('/api/test', function(req, res){
+	res.send('Hello world')
+})
+
 app.listen(3000, function () {
 	console.log("app is listening on port 3000")
 })

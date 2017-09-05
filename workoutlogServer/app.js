4e9fe0144489c588773f51fdb922ead36b7c7bde
Isaac Ludlow <isaac@theludlows.org>
2017-09-05 16:42:57 -0400

    Connected to postgres

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,12 +1,80 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-app.use(require('./middleware/headers'))
+app.use(require('./middleware/headers'));
 
 app.use('/api/test', function(req, res){
-	res.send('Hello world')
-})
+	res.send("Hello World");
+});
 
-app.listen(3000, function () {
-	console.log("app is listening on port 3000")
-})
+app.listen(3000, function(){
+	console.log('App is listening on 3000.')
+});
+
+
+
+
+
+
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'iLovepostgres', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
+
+
+
+sequelize.authenticate().then(
+	function() {
+		console.log('connected to workoutlog postgres db');
+	},
+	function(err){
+		console.log(err);
+	}
+);
+
+
+//Data Model
+var User = sequelize.define('user', {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING,
+});
+
+User.sync();
+
+/* 
+***DANGER: THIS WILL DROP (DELETE) THE USER TABLE***
+User.sync({ force: true }); 
+*/
+
+
+app.use(bodyParser.json());
+
+app.post('/api/user', function(req, res) {
+		var user = req.body.user.username;
+		var pass = req.body.user.password;
+		//Need to create a user object and use sequelize to put that user into
+		//
+
+		User.create({
+			username: user,
+			passwordhash: pass
+		}).then(
+		//Sequelize is going to return the object it created from db.
+
+			function createSuccess(user){
+				res.json({
+						user: user,
+						message: 'you did it!!!'
+				});
+			},
+			function createError(err){
+				res.send(500, err.message);
+
+			}
+		);
+	});
+
+
--- workoutlogServer/middleware/headers.js
+++ workoutlogServer/middleware/headers.js
@@ -1,4 +1,4 @@
-module.exports = function (req, res, next) {
-	res.header('access-control-allow-origin', '*')
-	next()
+module.exports  = function (req, res, next) /*Not exactly sure what the perameters are doing...*/{
+	res.header('access-control-allow-origin', '*')  //This line says we're allowing commmunication from any origin/server to any other origin/server
+	next() //The "next" key word is a built in key word saying, if a file isn't found, move on to the next file
 }

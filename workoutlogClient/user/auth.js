8f3cbd790c12b9aa94df67684b1322cc45756f28
Isaac Ludlow <isaac@theludlows.org>
2017-09-08 11:30:41 -0400

    Connected online form with postgres database

--- workoutlogClient/user/auth.js
+++ workoutlogClient/user/auth.js
@@ -1,45 +1,84 @@
-$(function () {
-	$.extend(WorkoutLog {
-		//signup method
-		signup: function() {
-			//username & password varibles
-			var username = $("#su_username").val()
-			var password = $("#su_password").val()
-			//user
-			var user = {
-				user: {
-					username:username,
-					password: password
-				}
-			}
+$(function() {
+   $.extend( WorkoutLog, {
+      signup: function() {
+            var username = $("#su_username").val();
+            var password = $("#su_password").val();
+            var user = {user: {username: username, password: password }};
+            var signup = $.ajax({
+               type: "POST", 
+               url: WorkoutLog.API_BASE + "user", 
+               data: JSON.stringify(user), 
+               contentType: "application/json"
+            });
 
-			//signup post
-			var signup = $.ajax({
-				type: 'POST',
-				url: WorkoutLog.API_BASE + "user",
-				data: JSON.stringify(user),
-				contentType: "application/json"
-			})
-			//signup done/fail
-			signup.done(function(data) {
-				if(data.sessionToken) {
-					WorkoutLog.setAuthHeader(data.sessionToken)
-				}
 
-				$("#signup-modal").modal("hide")
-				$(".disable").removeClasss("disabled")
-				$("#logout").text("Logout")
-				}).fail(function() {
-					$("#su_error").text("There was an issue with the sign up").show()
-				})
-			})
-		}
+             
+            signup
+            //.done() Promise
+            //Runs asynchronously
+            .done(function(data) {
+               if (data.sessionToken) {
+                  WorkoutLog.setAuthHeader(data.sessionToken);
+                  console.log("You made it!");
+                  console.log(data.sessionToken);
+               }
 
-		//login method
+               $("#signup-modal").modal("hide");
+               $(".disabled").removeClass("disabled");
+             $("#loginout").text("Logout");
+               //go to define tab
+             $('.nav-tabs a[href="#define"]').tab('show');
+            })
 
-		//logout method
-	})
+            //.fail() Promise
+            .fail(function() {
+               $("#su_error").text("There was an issue with your username").show();
+            });
+      },
 
-	//bind events
-	$("#signup").on(click, WorkoutLog.signup)
-})		
+      //login
+
+     //  login: function() {
+   		// var username = $("#li_username").val();
+   		// var password = $("#li_password").val();
+   		// var user = {user:  {username: username, password: password }};
+   		// var login = $.ajax({
+   		// 	type: "POST", 
+   		// 	url: WorkoutLog.API_BASE + "login", 
+   		// 	data: JSON.stringify(user), 
+   		// 	contentType: "application/json"
+   		// });
+   		// login.done(function(data) {
+   		// 	if (data.sessionToken) {
+     //           WorkoutLog.setAuthHeader(data.sessionToken);
+               
+   		// 	}
+   		// 	// TODO: add logic to set user and auth token	
+   		// 	$("#login-modal").modal("hide");
+   		// 	$(".disabled").removeClass("disabled");
+   		// 	$("#loginout").text("Logout");
+   		// })
+   		// .fail(function() {
+   		// 	$("#li_error").text("There was an issue with your username or password").show();
+     //  		});
+     //  },
+
+      //logout
+      // loginout: function() {
+      //    if (window.localStorage.getItem("sessionToken")) {
+      //       window.localStorage.removeItem("sessionToken");
+      //       $("#loginout").text("Login");
+      //    }
+      // }
+   });
+
+   // bind events
+  // $("#login").on("click", WorkoutLog.login);
+   $("#signup").on("click", WorkoutLog.signup);
+ //  $("#loginout").on("click", WorkoutLog.loginout);
+
+   // if (window.localStorage.getItem("sessionToken")) {
+   //    $("#loginout").text("Logout");
+   // }
+
+});
